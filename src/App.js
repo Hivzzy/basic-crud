335101a12fdd73f85405991880834fd8a6cd312d
Hivzzy <114140117+Hivzzy@users.noreply.github.com>
2024-07-19 21:05:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Typography, CssBaseline, Box } from '@mui/material';
+import DataTable from './components/DataTable';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="main" maxWidth="lg">
+      <CssBaseline />
+      <Box 
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: 8,
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          Data Table with Direct Manipulation
+        </Typography>
+        <DataTable />
+      </Box>
+    </Container>
   );
 }
 
